cf710c79cda787fc5cc663244ae28ae6e9e848db
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-04-11 20:43:35 -0400

    Add Code - Added Asteroid logic where the sprite will spawn with sprites - Changed Prefabs for asteroids
    * = Note to self
    
    * Going to add a bullet sprite

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -18,9 +18,22 @@ public class AsteroidSpawner : MonoBehaviour
             float angle = Random.Range(0, Mathf.PI * 2);
             Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * 45;
             GameObject newAsteroid = Instantiate(largeAsteroid, pos, Quaternion.identity);
+
+
+            // copy the sprite and color 
+            SpriteRenderer prefabSR = largeAsteroid.GetComponent<SpriteRenderer>();
+            SpriteRenderer newSR = newAsteroid.GetComponent<SpriteRenderer>();
+            if (prefabSR != null && newSR != null)
+            {
+                newSR.sprite = prefabSR.sprite;
+                newSR.color = prefabSR.color;
+            }
+
             Asteroid asteroid = newAsteroid.GetComponent<Asteroid>();
             asteroid.RandomizeStartVelocity = false;
             asteroid.MoveTowardsCentre();
         }
     }
+    
 }
+
