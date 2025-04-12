938ce1416c519d620a58e997f745804622e58730
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-04-11 20:29:11 -0400

    Add Code - Add Win Manager NOTE TO FUTURE SELF: Please add more code comments here, I kind of just started coding and didn't explain myself well enough!

--- Assets/Scripts/Brendans Scripts/WinManager.cs
+++ Assets/Scripts/Brendans Scripts/WinManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 public class WinManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text winDisplay;
+    private bool hasWon = false;
+
     void Start()
     {
-        
+        if (winDisplay != null)
+        {
+            winDisplay.enabled = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!hasWon && GameObject.FindGameObjectsWithTag("Asteroid").Length == 0) // If the player destroys all the asteroids then you win! 
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        if (winDisplay != null)
+        {
+            winDisplay.enabled = true;
+        }
+        hasWon = true;
     }
 }
